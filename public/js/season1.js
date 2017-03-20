e663e964297a33d8fb0ba56c5edb07d3afc123f0
itskobelong <itskobelong@yahoo.com>
2017-03-20 14:49:12 +0800

    1st

---
+++ public/js/season1.js
@@ -0,0 +1,44 @@
+$('#app').html(`
+
+<div class="spinner"></div>
+<div class="spinner-wrapper">
+	<div class="spinner"></div>
+`);
+$('#preLoader ').hide();
+setTimeout(function(){
+	x();
+},1000);
+
+function x(){
+$.ajax({
+	url: "http://www.omdbapi.com/?i=tt3205802"
+}).done(function(res){
+	console.log(res);
+	let html = `
+	   <center>
+		<h1>${res.Title}</h1>
+		<h1>Season 1</h1>
+		<img width=250 height=300 src="${res.Poster}"/>
+		
+		<div id="container">
+		<div id="left">
+		
+		</div>
+		<h3>Type: ${res.Type}</h3>
+		<h3>Released: ${res.Released}</h3>
+		<h3>Runtime: ${res.Runtime}</h3>
+		<h3>Genre: ${res.Genre}</h3>
+		<h3>Director: ${res.Director}</h3>
+		<h3>Writer: ${res.Writer}</h3>
+		<h3>Actors: ${res.Actors}</h3>
+		<h3>Plot: ${res.Plot}</h3>
+		<h3>Language: ${res.Language}</h3>
+		<h3>Country: ${res.Country}</h3>
+		<h3>Awards: ${res.Awards}</h3>
+</div>
+		
+	`;
+	$('#app').html(html);
+
+});
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+//server.js
+//require modules
+var express = require('express');
+var path = require('path');
+//instantiate express
+var app = express();
+//set port
+app.set('port', (process.env.PORT || 5000));
+//use static files
+app.use(express.static(path.join(__dirname, 'public')));
+//express routes
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/htgawmwebsite.html'));
+});
+app.get('/season1.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/season1.html'));
+});
+app.get('/season2.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/season2.html'));
+});
+app.get('/season3.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/season3.html'));
+});
+app.get('/season4.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/season4.html'));
+});
+//express server listen
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ',app.get('port'));
+});
